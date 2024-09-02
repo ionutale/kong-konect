944436c2661b71f5ac8263d7ba2956e7118f24e6
Ion Utale <iutale@florence-consulting.it>
2024-09-02 10:06:45 +0200

    added api routes and handlers

--- kong-apis-portal-golang/main.go
+++ kong-apis-portal-golang/main.go
@@ -14,9 +14,14 @@ func main() {
 
 	api := app.Group("/api")                        // /api
 	v1 := api.Group("/v1")                          // /api/v1
-	v1.Get("/organizations", handler_organizations) // /api/v1/user
+	v1.Get("/organizations", handler_organizations) // /api/v1/organizations
+	v1.Get("/services", handler_services)           // /api/v1/services
+	v1.Get("/services/:id", handler_serviceById)       // /api/v1/services/:id
+	v1.Get("/services/:id/versions/:versionId", handler_serviceVersionById) // /api/v1/services/:id/versions/:versionId
+
 	v1.Get("/users", handler_users)                 // /api/v1/user
-	v1.Get("/services", handler_services)           // /api/v1/user
+	v1.Post("/users/:id/login", handlerUserLogin)   // /api/v1/user/:id/login
+	v1.Post("/users/:id/password", handlerUserSetPassword) // /api/v1/user/:id/set-password
 
 	app.Listen(":3000")
 }
@@ -25,12 +30,25 @@ func handler_organizations(c *fiber.Ctx) error {
 	return c.SendString("List of organizations")
 }
 
+
+// services
+func handler_services(c *fiber.Ctx) error {
+
+	return c.SendString("List of services")
+}
+
+
+
+// users
 func handler_users(c *fiber.Ctx) error {
 
 	return c.SendString("List of users")
 }
 
-func handler_services(c *fiber.Ctx) error {
+func handlerUserLogin(c *fiber.Ctx) error {
+	return c.SendString("User login")
+}
 
-	return c.SendString("List of services")
+func handlerUserSetPassword(c *fiber.Ctx) error {
+	return c.SendString("User set password")
 }
